ed7e61b9e23eb88d58f3f2fd6e0ab281dbe86bb0
qalmee <komarevtsev24@gmail.com>
2018-11-12 20:53:11 +0700

    finish states now works just fine!

--- tyap_second_lab/mainwindow.cpp
+++ tyap_second_lab/mainwindow.cpp
@@ -87,10 +87,7 @@ void MainWindow::checkChainSlot()
         }
     }    
     chainString = chainLine->text();
-    if (chainString == ""){
-        this->badInputSlot();
-        return;
-    }
+
     this->runMachine();
 }
 
@@ -283,9 +280,13 @@ void MainWindow::checkStatesSlot()
 
 void MainWindow::runMachine()
 {
+    QVector<bool> finishMask(numberOfStates);
+    for (int i = 0; i<numberOfStates; i++){
+        finishMask[i] = (modelComboBox->item(i)->checkState() == Qt::Checked);
+    }
     machine->setChain(chainString);
     machine->setData(&statesNames, &terminals, &statesValues,
-                     statesNames.indexOf(startState->currentText()), statesNames.indexOf(finishState->currentText()));
+                     statesNames.indexOf(startState->currentText()), finishMask);
     machine->run();
     int error = machine->getError();
     auto result = machine->getResult();

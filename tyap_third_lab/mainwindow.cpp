21ee465b41bdf34e4c1c9da8ba585bf44e5bab58
LeshaTheSlayer <muhomorcheg@gmail.com>
2018-11-14 18:33:10 +0700

    front

--- tyap_third_lab/mainwindow.cpp
+++ tyap_third_lab/mainwindow.cpp
@@ -3,9 +3,70 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    numberOfStatesLabel = new QLabel("Множество состояний:");
+    numberOfFinishStatesLabel = new QLabel("Множество кон. состояний:");
+    inputAlphabetLabel = new QLabel("Входной алфавит:");
+    stackAlphabetLabel = new QLabel("Алфавит стека:");
+    chainLabel = new QLabel("Цепочка для проверки:");
+    startStateLabel = new QLabel("Начальное состояние:");
+    emptySymbolLabel = new QLabel("Пустой символ:");
+    startStackLabel = new QLabel("Начальный стек:");
+    machineLabel = new QLabel("Автомат:");
+    outputLabel = new QLabel("Вывод:");
+
+    numberOfStatesLine = new QLineEdit;
+    numberOfFinishStatesLine = new QLineEdit;
+    inputAlphabetLine = new QLineEdit;
+    stackAlphabetLine = new QLineEdit;
+    chainLine = new QLineEdit;
+    startStateLine = new QLineEdit;
+    emptySymbolLine = new QLineEdit;
+    startStackLine = new QLineEdit;
+
+    machineTextEdit = new QTextEdit;
+    outputTextEdit = new QTextEdit;
+
+    checkChainButton = new QPushButton("Начать проверку");
+
+    inputLayout = new QGridLayout;
+
+    inputLayout->addWidget(numberOfStatesLabel, 0, 0);
+    inputLayout->addWidget(numberOfFinishStatesLabel, 1, 0);
+    inputLayout->addWidget(inputAlphabetLabel, 2, 0);
+    inputLayout->addWidget(stackAlphabetLabel, 3, 0);
+    inputLayout->addWidget(chainLabel, 4, 0);
+    inputLayout->addWidget(numberOfStatesLine, 0, 1);
+    inputLayout->addWidget(numberOfFinishStatesLine, 1,1);
+    inputLayout->addWidget(inputAlphabetLine, 2, 1);
+    inputLayout->addWidget(stackAlphabetLine, 3, 1);
+    inputLayout->addWidget(chainLine, 4, 1);
+    inputLayout->addWidget(startStateLabel, 0, 2);
+    inputLayout->addWidget(emptySymbolLabel, 1, 2);
+    inputLayout->addWidget(startStackLabel, 2, 2);
+    inputLayout->addWidget(startStateLine, 0, 3);
+    inputLayout->addWidget(emptySymbolLine, 1, 3);
+    inputLayout->addWidget(startStackLine, 2, 3);
+    inputLayout->addWidget(checkChainButton, 4, 3);
+
+    machineLayout = new QVBoxLayout;
+    outputLayout = new QVBoxLayout;
+    textEditsLayout = new QHBoxLayout;
+    wholeLayout = new QVBoxLayout;
+
+    machineLayout->addWidget(machineLabel);
+    machineLayout->addWidget(machineTextEdit);
+    outputLayout->addWidget(outputLabel);
+    outputLayout->addWidget(outputTextEdit);
+    textEditsLayout->addLayout(machineLayout);
+    textEditsLayout->addLayout(outputLayout);
+    wholeLayout->addLayout(inputLayout);
+    wholeLayout->addLayout(textEditsLayout);
+
+    window = new QWidget();
+    setCentralWidget(window);
+    window->setLayout(wholeLayout);
 }
 
 MainWindow::~MainWindow()
 {
-
 }
--- tyap_third_lab/mainwindow.h
+++ tyap_third_lab/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QPushButton>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +17,40 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private:
+    QLabel *numberOfStatesLabel;
+    QLabel *numberOfFinishStatesLabel;
+    QLabel *inputAlphabetLabel;
+    QLabel *stackAlphabetLabel;
+    QLabel *chainLabel;
+    QLabel *startStateLabel;
+    QLabel *emptySymbolLabel;
+    QLabel *startStackLabel;
+    QLabel *machineLabel;
+    QLabel *outputLabel;
+
+    QLineEdit *numberOfStatesLine;
+    QLineEdit *numberOfFinishStatesLine;
+    QLineEdit *inputAlphabetLine;
+    QLineEdit *stackAlphabetLine;
+    QLineEdit *chainLine;
+    QLineEdit *startStateLine;
+    QLineEdit *emptySymbolLine;
+    QLineEdit *startStackLine;
+
+    QPushButton *checkChainButton;
+
+    QTextEdit *machineTextEdit;
+    QTextEdit *outputTextEdit;
+
+    QGridLayout *inputLayout;
+
+    QHBoxLayout *textEditsLayout;
+    QVBoxLayout *machineLayout;
+    QVBoxLayout *outputLayout;
+    QVBoxLayout *wholeLayout;
+
+    QWidget *window;
 };
 
 #endif // MAINWINDOW_H
